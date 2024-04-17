ae0cb50a417529f6834bcff2d57710b5d4e8fe63
GuyFriedF <156193153+GuyFriedF@users.noreply.github.com>
2024-04-17 18:07:41 +0200

    Update RecipeHeroApp.py

--- RecipeHeroApp.py
+++ RecipeHeroApp.py
@@ -2,7 +2,6 @@ import pandas as pd
 import csv
 import numpy as np
 import streamlit as st
-
 url = "https://drive.google.com/file/d/1OMZf5eUXRiagsSVQ_Q-ETt8_m3c33K-p/view?usp=sharing"
 path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df = pd.read_csv(path,engine='python', on_bad_lines='skip')
@@ -12,35 +11,26 @@ st.set_page_config(layout="wide")
 st.markdown("""
 <style>
 .big-font {
-    font-size:800% !important;
+    font-size:200px !important;
 }
 .stMultiSelect > label {
-            font-size:400%; 
+            font-size:200%; 
             font-weight:bold; 
-            color:black
-            text-align: center;
+            color:black;
 </style>
 """, unsafe_allow_html=True)
 
 
 
 
-
 #Creating lists in the dataframe for better matching:
-
 listy=[]   #empty list to be filled with ingredeints names
-
 for i in range(len(df)):
   x = df.iloc[i]['ingredients'].strip("[]").replace(r'"',"").replace(r"'","").split(", ")  #using the split method to create a list of ingredients
   listy.append(x)  #appending each list to the empty list, creating a list of lists
-
 df['ingredients_list']=listy   #adding the new column to the database
-
 # Adding an "Ingredients" column that is composed of lists to the database, for easier matching
-
-
 listx=[]   #empty list to be filled different recipe steps:
-
 for i in range(len(df)):
   listz=[]
   x = df.iloc[i]['steps'].strip("[]").split("', '")  #using the split method to create a list of ingredients
@@ -48,53 +38,38 @@ for i in range(len(df)):
     y = j.replace(r'"',"").replace(r"'","")
     listz.append(y)
   listx.append(listz)  #appending each list to the empty list, creating a list of lists
-
 df['steps_list']=listx   #adding the new column to the database
-
 #Creating 'Dietary' and 'Gluten' labels, and adding them to the database:
-
 df['dietary'] = 'vegan'    #Assuming all recipes are 'Vegan', unless they contain animal products, then they become either 'Vegeterian' or 'None' (meaning a person has no dietary restrictions/Omnivore)
 df['gluten'] = 'gluten-free'
-
 diet_list = {"vegeterian":['butter','milk','whole milk','cream','cheese','feta','eggs','parmesan',
              'buttermilk','mayonaise','mayonnaise','yoghurt'],
              "none":['beef','sirloin','chicken','fish','shrimp','octopus','ground beef',
              'bacon','salmon','lamb','pork','sausage','anchovies','meat','crab',' ham'],
             }
 not_glutenfree = ['bread','pasta','flour','pizza','crust','cracker','wheat']   #If any recipe contains these ingredients, they are not Gluten-Free
-
 for i in diet_list:     #Iterating over the 'Dietary' dictionary, looking whether any recipe in the database contains any of the values inside in each key
   for j in diet_list[i]:
     df.loc[df['ingredients'].str.contains(j), 'dietary'] = i
     df.loc[df['name'].str.contains(j), 'dietary'] =i
-
-
 for i in not_glutenfree:
   df.loc[df['ingredients'].str.contains(i), 'gluten'] = "not gluten-free"
   df.loc[df['name'].str.contains(i), 'gluten'] = "not gluten-free"
 # Turning string-based 'dietary' column to lists
-
 rest_list=[]   #empty list to be filled
-
 for i in range(len(df)):
   a = df.iloc[i]['dietary'].split(",")  #using the split method to create a list of dietary restrictions
   b= set(a)
   c= list(b)
   rest_list.append(c)  #appending each list to the empty list, creating a list of lists
-
 df['dietary']=rest_list
-
-
 #Creating a list of all unique ingredeints in the recipes database, sorting them alphabetically, to be used in the multiselect drop-down menu
 temp=df['ingredients_list'].explode().unique()
 temp2 = list(temp)
 temp3 = np.sort(temp2)
 allingred = list(temp3)
 #this list will be used to make sure that user input has to match available ingredeints in the database
-
-
 #Setting values to session state:
-
 if 'currentdf' not in st.session_state:
   st.session_state.currentdf = df
 if 'min_n' not in st.session_state:
@@ -109,21 +84,14 @@ if 'min_time' not in st.session_state:
   st.session_state.min_time = 1
 if 'max_time' not in st.session_state:
   st.session_state.max_time = 200
-
 df1 = ""
-
-
-
 #Define some functions to be used in the script
-
 def df_update(df):
   st.session_state.currentdf = df
-
 def minmax(df, column):
   min=df[column].min()
   max=df[column].max()
   return min, max
-
 def match_ingredients(df, igdlist):
   var=len(igdlist)
   temp = df[df['ingredients_list'].apply(lambda x: any(ingredient in igdlist for ingredient in x))]   #check if any ingredient from user-input is found in ingredeints list, per each recipe
@@ -132,68 +100,55 @@ def match_ingredients(df, igdlist):
   result = result.sort_values('matches', ascending=False)
   df_update(result)
   return result
-
 def match_style(df,params, restrict, gluten):
   result=df[df['style'].apply(lambda x: any(ingredient in x for ingredient in params))]
   result2 = result[result['dietary'].apply(lambda x: any(ingredient in x for ingredient in restrict))]
   result3 = result2[result2['gluten'].apply(lambda x: any(ingredient in x for ingredient in gluten))]
   df_update(result3)
   return result3
-
 def match_dietary(df,params):
   result=df[df['dietary'].apply(lambda x: any(ingredient in x for ingredient in params))]
   df_update(result)
   return result
-
-
 def match_steps(df, values):
   result=df.loc[df['n_steps'].between(values[0],values[1])]
   result = result.sort_values('matches', ascending=False)
   df_update(result)
   return result
-
 def match_num(df,values):
   result=df.loc[df['n_ingredients'].between(values[0],values[1])]
   df_update(result)
   return result
-
 def match_time(df, values):
   result=df.loc[df['minutes'].between(values[0],values[1])]
   df_update(result)
   return result
 
 
-
-
 # Title
+st.title("RecipeHero!")
 #st.title("RecipeHero!")
 st.markdown("<h1 style='text-align: center; color: black;'>RecipeHero!</h1>", unsafe_allow_html=True)
 
 # Dropdown menu
 with st.container():
-  st.markdown("<h2 style='text-align: center; color: black;'>Ingredients</h2>", unsafe_allow_html=True)
+  st.subheader("Ingredients:")
   st.session_state.currentdf = df
-  st.markdown("<h7 style='text-align: end; color: black;'>Please type in your ingredients:</h7>", unsafe_allow_html=True)
-  dropdown_value = st.multiselect("", allingred, on_change=df_update, args=[df])
+  dropdown_value = st.multiselect("Please type in your ingredients:", allingred, on_change=df_update, args=[df])
   st.session_state.currentdf = match_ingredients(df, dropdown_value)
   df2 =df1
-
-
 #st.session_state
-
 stylist=['italian', 'french',
   'mexican','asian', 'american', 'oriental',
   'greek', 'spanish', 'soup','breakfast','salad', 
   'dessert','bread','drinks','dips & dressing']
 with st.container():
-  st.markdown("<h2 style='text-align: center; color: black;'>Style and Dietary-restrictions</h2>", unsafe_allow_html=True)
-  st.markdown("<h7 style='text-align: center; color: black;'>Please choose which style of recipes you would like:</h7>", unsafe_allow_html=True)
-  params = st.multiselect("", ['italian', 'french',
+  st.subheader("Style and Dietary-restrictions:")
+  params = st.multiselect("Please choose which style of recipes you would like:", ['italian', 'french',
   'mexican','asian', 'american', 'oriental',
   'greek', 'spanish', 'soup','breakfast','salad', 
   'dessert','bread','drinks','dips & dressing','any'],default=['any'], on_change=df_update, args=[st.session_state.currentdf])
-  st.markdown("<h7 style='text-align: center; color: black;'>Do you have any dietary restrictions?</h7>", unsafe_allow_html=True)
-  restrict = st.multiselect("", ['none','vegan','vegeterian', 'gluten-free'], default=['none'], on_change=df_update, args=[st.session_state.currentdf])
+  restrict = st.multiselect("Do you have any dietary restrictions?", ['none','vegan','vegeterian', 'gluten-free'], default=['none'], on_change=df_update, args=[st.session_state.currentdf])
   if 'gluten-free' in restrict:
     if params==['any']:
       try:
@@ -217,9 +172,8 @@ with st.container():
       except:
         st.write("Waiting for values...")
   df2=df1
-
 with st.container():
-  st.markdown("<h2 style='text-align: center; color: black;'>Number of ingredients, steps and time:</h2>", unsafe_allow_html=True)
+  st.subheader("Number of ingredients, steps and time:")
   col1, col2, col3 = st.columns(3)
   with col1:
     try:
@@ -247,7 +201,6 @@ with st.container():
       tempvalues2 = st.slider(
       'Select the number of steps',
       min_value=1, max_value=25,value=[1,25])
-
   with col3:
     try:
       st.session_state.min_time, st.session_state.max_time = minmax(st.session_state.currentdf,'minutes')
@@ -261,16 +214,9 @@ with st.container():
       tempvalues3 = st.slider(
       'Select the number of minutes',
       min_value=0, max_value=300,value=[1,300])
-
-
-
-
-
 with st.container():
-  st.markdown("<h2 style='text-align: center; color: black;'>Recipes Results:</h2>", unsafe_allow_html=True)
-  st.markdown("<h7 style='text-align: center; color: black;'>How many recipes should be shown:</h7>", unsafe_allow_html=True) 
-  number = st.number_input("",min_value=1)
-
+  st.subheader("Recipes results:")
+  number = st.number_input("How many recipes should be shown",min_value=1)
   button = st.button("Show results")
   if button:
     if number<len(st.session_state.currentdf.index):
@@ -279,14 +225,12 @@ with st.container():
       for i in list(range(number)):
         tabstr = df2.iloc[i]['name']
         tablist.append(tabstr)
-
     else:
       tablist = []
       df2 = st.session_state.currentdf
       for i in list(range(len(df2.index))):
         tabstr = df2.iloc[i]['name']
         tablist.append(tabstr)
-
     try:
       tabs =  st.tabs(tablist)
       for i,tab in enumerate(tabs):
